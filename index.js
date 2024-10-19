17a7fc4ee6b89952fc4c8fe6c2c51cf6524db480
Anatoliy2024 <gredlend20@gmail.com>
2024-10-19 12:08:01 +0300

    some change

--- index.js
+++ index.js
@@ -19,36 +19,113 @@ app.use(
 app.get("/", (req, res) => {
   res.send("Привет, Мир")
 })
+// app.get("/api/clinic", async (req, res) => {
+
+//   try {
+//     const { data: html } = await axios.get(
+//       "https://glazahelp.ru/regiony-kliniki/"
+//     )
+//     const $ = cheerio.load(html)
+//     let clinics = []
+
+//     const linkRegions = $(".overlay_image_box")
+//       .map((index, elem) => {
+//         return $(elem).attr("data-link")
+//       })
+//       .get()
+
+//     const regionPromises = linkRegions.map(async (linkRegions) => {
+//       return limit(async () => {
+//         try {
+//           const { data: htmlRegion } = await axios.get(linkRegions, {
+//             timeout: 20000,
+//           })
+//           const $ = cheerio.load(htmlRegion)
+
+//           const clinicsLinks = $(".left a")
+//             .map((i, el) => {
+//               clinics.push([$(el).attr("href"), $(el).text()])
+//               return $(el).attr("href")
+//             })
+//             .get()
+
+//           const clinicPromise = clinicsLinks.map(async (clinicLink) => {
+//             console.log("clinicLink", clinicLink)
+//             try {
+//               const { data: clinicData } = await axios.get(clinicLink, {
+//                 timeout: 20000,
+//               })
+//               const $ = cheerio.load(clinicData)
+//               const fullText = $("div.text.text_block")
+//                 .filter(function () {
+//                   return $(this).text().includes("Адрес")
+//                 })
+//                 .first()
+//                 .text()
+
+//               const regex = /Адрес\s*:\s*([\s\S]*?)\s*Телефон/
+//               const address = fullText.match(regex)
+
+//               clinics = clinics.map((el) => {
+//                 if (el[0] === clinicLink) {
+//                   return [...el, address[1]]
+//                 } else {
+//                   return el
+//                 }
+//               })
+
+//               console.log("clinics", clinics)
+//             } catch (e) {
+//               console.log(e)
+//             }
+//           })
+//           // res.json(clinics)
+//           await Promise.all(clinicPromise)
+//         } catch (e) {
+//           console.log(e)
+//         }
+//       })
+//     })
+
+//     await Promise.all(regionPromises)
+//     res.json(clinics)
+//   } catch (e) {
+//     console.log(e)
+//     res.status(500).send("Error occurred")
+//   }
+// })
+
 app.get("/api/clinic", async (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream")
+  res.setHeader("Cache-Control", "no-cache")
+  res.setHeader("Connection", "keep-alive")
+
   try {
     const { data: html } = await axios.get(
       "https://glazahelp.ru/regiony-kliniki/"
     )
     const $ = cheerio.load(html)
-    let clinics = []
-
     const linkRegions = $(".overlay_image_box")
-      .map((index, elem) => {
-        return $(elem).attr("data-link")
-      })
+      .map((index, elem) => $(elem).attr("data-link"))
       .get()
-
-    const regionPromises = linkRegions.map(async (linkRegions) => {
-      return limit(async () => {
+    let clinics = {}
+    for (const linkRegion of linkRegions) {
+      // Изменил здесь с linkRegions на linkRegion
+      await limit(async () => {
         try {
-          const { data: htmlRegion } = await axios.get(linkRegions, {
+          const { data: htmlRegion } = await axios.get(linkRegion, {
             timeout: 20000,
           })
           const $ = cheerio.load(htmlRegion)
-
+          // const clinicsLinks = $(".left a").map((i, el) => $(el).attr("href")).get();
           const clinicsLinks = $(".left a")
             .map((i, el) => {
-              clinics.push([$(el).attr("href"), $(el).text()])
+              clinics[$(el).attr("href")] = $(el).text()
               return $(el).attr("href")
             })
             .get()
 
-          const clinicPromise = clinicsLinks.map(async (clinicLink) => {
+          for (const clinicLink of clinicsLinks) {
             console.log("clinicLink", clinicLink)
             try {
               const { data: clinicData } = await axios.get(clinicLink, {
@@ -65,34 +142,31 @@ app.get("/api/clinic", async (req, res) => {
               const regex = /Адрес\s*:\s*([\s\S]*?)\s*Телефон/
               const address = fullText.match(regex)
 
-              clinics = clinics.map((el) => {
-                if (el[0] === clinicLink) {
-                  return [...el, address[1]]
-                } else {
-                  return el
-                }
-              })
-
-              console.log("clinics", clinics)
+              // Отправляем данные клиенту по мере их обработки
+              res.write(
+                `data: ${JSON.stringify({
+                  link: clinicLink,
+                  city: clinics[clinicLink],
+                  address: address[1],
+                })}\n\n`
+              )
             } catch (e) {
               console.log(e)
             }
-          })
-          // res.json(clinics)
-          await Promise.all(clinicPromise)
+          }
         } catch (e) {
           console.log(e)
         }
       })
-    })
-
-    await Promise.all(regionPromises)
-    res.json(clinics)
+    }
   } catch (e) {
     console.log(e)
     res.status(500).send("Error occurred")
+  } finally {
+    res.end()
   }
 })
+
 export default app
 // app.listen(5001, () => {
 //   console.log("Server is running PORT 5000")
